33ffdd78bef3512c027878eccf1335a976c22564
durumyisking <cww9512@gmail.com>
2025-07-02 00:46:21 +0900

    ---Objective--- - 커스텀 모듈 생성 - 커스텀 로그 카테고리 추가
    ---Key Changes---
    - 언리얼 기본 모듈을 사용하지 않고 FLCGameModule모듈을 만들어서 Primary 모듈로 설정
    - 커스텀 로그 매크로를 넣을 LCLog파일을 따로 추가 및 LogLC라는 커스텀 로그 카테고리 추가

--- Source/LyraClone/LyraClone.cpp
+++ Source/LyraClone/LyraClone.cpp
@@ -2,5 +2,29 @@
 
 #include "LyraClone.h"
 #include "Modules/ModuleManager.h"
+#include "LCLog.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LyraClone, "LyraClone" );
+class FLCGameModule : public FDefaultGameModuleImpl
+{
+public:
+
+	// IModuleInterface내 함수들 상속
+	virtual void StartupModule() override; 
+	virtual void ShutdownModule() override;
+
+};
+
+
+void FLCGameModule::StartupModule()
+{
+	UE_LOG(LogLC, Warning, TEXT("Startup Moudle"));
+}
+
+void FLCGameModule::ShutdownModule()
+{
+	UE_LOG(LogLC, Warning, TEXT("Shutdown Moudle"));
+}
+
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FLCGameModule, LyraClone, "LyraClone" );
